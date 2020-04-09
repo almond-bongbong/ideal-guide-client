cedaf4337329272288d17a7386f3264a6c8158a2
cmlee <bal.dongdongdong@gmail.com>
2020-04-10 01:03:53 +0900

    bug fix

--- pages/party/policy/[partyName].tsx
+++ pages/party/policy/[partyName].tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { getParties, getPartyPolicy } from '../../../api/internal/election';
-import { Party } from '../../../interfaces';
+import { GetServerSideProps } from 'next';
+import { getPartyPolicy } from '../../../api/internal/election';
 import styled from 'styled-components';
 import colorByParty from '../../../constants/colorByParty';
 import PartyPolicySelection from '../../../components/party/PartyPolicySelection';
+import { useRouter } from 'next/router';
 
 interface Props {
   policy: {
@@ -13,7 +13,7 @@ interface Props {
 }
 
 const Container = styled.div`
-  padding: 30px;
+  padding: 30px 30px 50px;
   background-color: #fff;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
 `;
@@ -51,20 +51,27 @@ const PolicyItem = styled.li`
   }
 `;
 
+const EmptyPolicy = styled.div`
+  font-size: 18px;
+`;
+
 function PartyName({ policy }: Props) {
   const [activeRealmId, setActiveRealmId] = useState(0);
-  const realmNames = [
-    policy.prmsRealmName1,
-    policy.prmsRealmName2,
-    policy.prmsRealmName3,
-    policy.prmsRealmName4,
-    policy.prmsRealmName5,
-    policy.prmsRealmName6,
-    policy.prmsRealmName7,
-    policy.prmsRealmName8,
-    policy.prmsRealmName9,
-    policy.prmsRealmName10,
-  ].filter(Boolean);
+  const router = useRouter();
+  const realmNames = policy
+    ? [
+        policy.prmsRealmName1,
+        policy.prmsRealmName2,
+        policy.prmsRealmName3,
+        policy.prmsRealmName4,
+        policy.prmsRealmName5,
+        policy.prmsRealmName6,
+        policy.prmsRealmName7,
+        policy.prmsRealmName8,
+        policy.prmsRealmName9,
+        policy.prmsRealmName10,
+      ].filter(Boolean)
+    : [];
   const realm = realmNames.map((r, i) => ({ id: i, name: r }));
 
   const handlePolicy = (id: number) => {
@@ -73,9 +80,10 @@ function PartyName({ policy }: Props) {
 
   return (
     <Container>
-      <Title color={colorByParty.get(policy.partyName)}>
-        {policy.partyName} 정책
+      <Title color={colorByParty.get(router.query.partyName)}>
+        {router.query.partyName} 정책
       </Title>
+
       <PartyPolicySelectionWrap>
         <PartyPolicySelection
           realm={realm}
@@ -83,29 +91,36 @@ function PartyName({ policy }: Props) {
           onClick={handlePolicy}
         />
       </PartyPolicySelectionWrap>
-      <PolicyList>
-        <PolicyItem>
-          <strong>{policy[`prmsTitle${activeRealmId + 1}`]}</strong>
-          <p>{policy[`prmmCont${activeRealmId + 1}`]}</p>
-        </PolicyItem>
-      </PolicyList>
+
+      {policy ? (
+        <PolicyList>
+          <PolicyItem>
+            <strong>{policy[`prmsTitle${activeRealmId + 1}`]}</strong>
+            <p>{policy[`prmmCont${activeRealmId + 1}`]}</p>
+          </PolicyItem>
+        </PolicyList>
+      ) : (
+        <EmptyPolicy>검색된 정책이 없습니다.</EmptyPolicy>
+      )}
     </Container>
   );
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const { data } = await getParties();
-  const paths = data.body.items.item.map((p: Party) => ({
-    params: { partyName: p.jdName },
-  }));
-  return { paths, fallback: false };
-};
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const { data } = await getParties();
+//   const paths = data.body.items.item.map((p: Party) => ({
+//     params: { partyName: p.jdName },
+//   }));
+//   return { paths, fallback: false };
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   try {
     if (params?.partyName) {
       const { data } = await getPartyPolicy(params.partyName as string);
-      return { props: { policy: data.body.items.item } };
+      if (data.body.items.item) {
+        return { props: { policy: data.body.items.item } };
+      }
     }
     return { props: {} };
   } catch (e) {
